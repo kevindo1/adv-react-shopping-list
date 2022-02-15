e5d9fd0cecee004f042879f3b0d5debc931429f9
Kevin Do <kevind0501@gmail.com>
2022-02-15 14:32:04 -0800

    added readme planning and file structure

---
+++ src/components/Add.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Add() {
+  return (
+    <div>
+      <input type="text" placeholder="Add Item"></input>
+      <button>Submit</button>
+    </div>
+  );
+}
---
+++ src/views/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Add from '../components/Add';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Shopping List</h1>
+      <Add />
+    </div>
+  );
+}
