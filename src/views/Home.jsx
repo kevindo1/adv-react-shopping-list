fb6156bde3f59767890ca1e032cbc714d82ca257
Kevin Do <kevind0501@gmail.com>
2022-02-15 16:00:34 -0800

    worked on add reducer in Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import Add from '../components/Add';
+import List from '../components/List';
+
+const initialItems = [
+  { id: 0, text: 'Bread 🥖', done: false },
+  { id: 1, text: 'Steak 🥩', done: false },
+  { id: 2, text: 'Potatoes 🥔', done: false },
+];
+
+function shoppingReducer(items, action) {
+  switch (action.type) {
+    case 'add': {
+      return [
+        ...items,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    default: {
+      throw Error(`Unknown type: ${action.type}`);
+    }
+  }
+}
 
 export default function Home() {
+  const [items, dispatch] = useReducer(shoppingReducer, initialItems);
+
+  const addItem = (text) => {
+    dispatch({
+      type: 'add',
+      id: items.length + 1,
+      text,
+    });
+  };
+
   return (
     <div>
       <h1>Shopping List</h1>
-      <Add />
+      <Add addItem={addItem} />
+      <List />
     </div>
   );
 }
