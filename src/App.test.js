67412cdc2a4a8573490b60e5dd95a5d175e9dd93
Kevin Do <kevind0501@gmail.com>
2022-02-16 17:39:18 -0800

    passing all behavioural tests

--- src/App.test.js
+++ src/App.test.js
@@ -11,23 +11,36 @@ test('renders shopping list with add/edit/delete', () => {
   const inputButton = screen.getByRole('button', { name: /submit/i });
 
   // add in new item
-  userEvent.type(inputBox, 'testing');
+  userEvent.type(inputBox, 'test');
   userEvent.click(inputButton);
 
   expect(screen.getByText(/test/i)).toBeInTheDocument();
 
   // EDIT TEST
   // get edit button
-  const editButton = screen.getByRole('button', { name: /edit bread 🥖/i });
+  const editButton = screen.getByLabelText('Edit test');
 
   // click on edit button
   userEvent.click(editButton);
 
   // get the input and save button
-  const editInput = screen.getByRole('textbox', { name: /edit bread 🥖/i });
-  const saveButton = screen.getByRole('button', { name: /save bread 🥖/i });
+  const editInput = screen.getByRole('textbox', { name: /edit test/i });
+  const saveButton = screen.getByRole('button', { name: /save test/i });
 
   // user event to edit
-  userEvent.type(editInput, '{selectall}{del}LETS GET THIS BREAD 🥖');
+  userEvent.type(editInput, '{selectall}{del}plswork');
   userEvent.click(saveButton);
+
+  screen.getByText('plswork');
+
+  expect(screen.queryByText('test')).not.toBeInTheDocument();
+
+  // DELETE TEST
+  // get delete button
+  const deleteButton = screen.getByLabelText('Delete plswork');
+
+  // user click delete button
+  userEvent.click(deleteButton);
+
+  expect(screen.queryByText('plswork')).not.toBeInTheDocument();
 });
