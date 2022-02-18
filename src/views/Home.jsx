1ddf13e1d2ca6d968838a5c5a408ae4d3769552c
Kevin Do <kevind0501@gmail.com>
2022-02-18 10:23:13 -0800

    added useList to home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Add from '../components/Add/Add';
 import List from '../components/List/List';
+import { useList } from '../context/Context';
 
 export default function Home() {
+  const { items, addItem, deleteItem, editItem } = useList();
+
   return (
     <div>
       <h1>Shopping List</h1>
